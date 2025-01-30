2bdbb82e7e82772f841bb295483775286a739a7f
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2025-01-30 21:08:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,38 @@
 
-import React from "react";
-import './../styles/App.css';
+// import React from "react";
+// import './../styles/App.css';
 
-const App = () => {
+// const App = () => {
+//   const [count,setCount] = useState(0)
+
+//   const onAdd = ()=>{
+//     setCount(count+1)
+//   }
+//   return (
+//     <div>
+//     <p>Button clicked {count} times</p>
+//     <button onClick={onAdd}>Click me</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React,{useState} from "react";
+
+
+const App = ()=>{
+
   const [count,setCount] = useState(0)
 
   const onAdd = ()=>{
     setCount(count+1)
   }
   return (
-    <div>
+    <div >
     <p>Button clicked {count} times</p>
     <button onClick={onAdd}>Click me</button>
     </div>
   )
 }
-
 export default App
